3820816f43aab4ca697dab85aa88c9ba515a6956
Sai Kiran <saikiran.madupu@gmail.com>
2023-03-15 16:07:21 -0700

    tbdf

--- simulator/access_pattern.py
+++ simulator/access_pattern.py
@@ -1,8 +1,7 @@
 import queue
 import random
 from datetime import datetime, timedelta
-
-import matplotlib.pyplot as plt
+from collections import Counter
 import numpy as np
 import pandas as pd
 import yaml
@@ -10,8 +9,8 @@ import yaml
 
 def generate_timestamps(start, end, distribution_size, max_peaks, min_sample_size, max_sample_size):
     # Converting to timestamp
-    start = start.timestamp()
-    end = end.timestamp()
+    start = int(start.timestamp())
+    end = int(end.timestamp())
 
     d = distribution_size
     peaks = 0
@@ -24,13 +23,24 @@ def generate_timestamps(start, end, distribution_size, max_peaks, min_sample_siz
         d -= sample_size
         max_peaks -= 1
         peaks += 1
-
         sigma = random.uniform(min_sample_size, max_sample_size)
         loc = random.uniform(start, end)
         # print('loc = ', loc, ' sample size = ', sample_size)
         sample = np.random.gumbel(loc=loc, scale=sigma, size=sample_size)
-        # print(sample.tolist())
+        # print('sample = ', len(sample))
         samples = samples + sample.tolist()
+
+    return samples
+
+
+def generate_timestamps_random(start, end, distribution_size, max_peaks, min_sample_size, max_sample_size):
+    d = distribution_size
+    samples = []
+
+    while d != 0:
+        t = random.uniform(start, end)
+        samples.append(t)
+        d -= 1
 
     return samples
 
@@ -40,15 +50,32 @@ class FileAccessData:
     request_timestamps = None
 
     def __init__(self):
-        self.request_timestamps = queue.Queue(maxsize=1024)
+        with open("config.yml", "r") as file:
+            data = yaml.full_load(file)
+        self.config = data.get("file_data")
+        self.request_timestamps = queue.Queue(self.config.get("qsize"))
 
-    def add_request(self, timestamp):
-        if self.request_timestamps.qsize() >= self.request_timestamps.maxsize:
-            print('limit reached')
-            return -1
-        self.request_timestamps.put(timestamp)
+    def add_requests(self, timestamps):
+        for timestamp in timestamps:
+            if self.request_timestamps.qsize() >= self.request_timestamps.maxsize:
+                print("Queue limit exceeded. Remaining requests are discarded")
+                break
+            self.request_timestamps.put(timestamp)
+            # print(type(timestamp))
         self.total_requests = self.request_timestamps.qsize()
-        return 0
+
+    def group_timestamps(self):
+        datetimes = sorted(list(self.request_timestamps.queue), reverse=True)
+
+        df = pd.DataFrame({"freq": datetimes})
+
+        # Group the datetimes into hourly intervals
+        df["interval"] = df["freq"].dt.floor("H")
+
+        # Get the frequency counts for each hour
+        grouped_df = df.groupby("interval").count()
+
+        return list(grouped_df.itertuples(index=True, name=None))
 
 
 class AccessPatternGenerator:
@@ -56,27 +83,16 @@ class AccessPatternGenerator:
         c = dict()
         with open("config.yml", "r") as file:
             c = yaml.full_load(file)
-        self.config = c
-        # print(self.config)
+        self.config = c.get("access_pattern")
 
     def generate_access_pattern(self):
-        start = datetime.now() - timedelta(days=1)
+        start = datetime.now() - timedelta(days=self.config.get("range"))
         end = datetime.now()
-        t = generate_timestamps(start=start,
-                                end=end,
-                                distribution_size=self.config.get("distribution_size"),
-                                max_peaks=self.config.get("max_peaks"),
-                                min_sample_size=self.config.get("min_sample_size"),
-                                max_sample_size=self.config.get("max_sample_size"))
+        timestamps = generate_timestamps_random(start=start,
+                                                end=end,
+                                                distribution_size=self.config.get("distribution_size"),
+                                                max_peaks=self.config.get("max_peaks"),
+                                                min_sample_size=self.config.get("min_sample_size"),
+                                                max_sample_size=self.config.get("max_sample_size"))
 
-        f = [1 for x in range(len(t))]
-        data = {"timestamps": t, "freq": f}
-        # Create DataFrame
-        df = pd.DataFrame(data)
-        df["timestamps"] = pd.to_datetime(df["timestamps"], unit='s')
-        df = df.resample(self.config.get("sampling_interval"), on='timestamps').sum()
-        df.sort_values(by=["timestamps"], inplace=True, ascending=False)
-        plt.plot(df)
-        plt.show()
-
-        return list(df.to_records())
+        return timestamps
--- simulator/weight_calculator.py
+++ simulator/weight_calculator.py
@@ -1,12 +1,19 @@
 import multiprocessing
-from access_pattern import AccessPatternGenerator
-from decay_function import compute_decay_factor
+from access_pattern import AccessPatternGenerator, FileAccessData
+from decay_function import DecayFunctionCalculator
 
 
 class WeightCalculator(multiprocessing.Process):
     def run(self) -> None:
         pattern_generator = AccessPatternGenerator()
-        # pattern = pattern_generator.generate_access_pattern()
-        # print(pattern)
-        f = compute_decay_factor(10, 0.05, 10)
-        print(f)
+        timestamps = pattern_generator.generate_access_pattern()
+
+        file_data = FileAccessData()
+        file_data.add_requests(timestamps)
+
+        grouped_timestamps = file_data.group_timestamps()
+
+        decay_function = DecayFunctionCalculator()
+        f = decay_function.compute_decay_factor(grouped_timestamps, 100, 120)
+        print("\n")
+        print("actual w = ", 100, ", adjusted w = ", f)
